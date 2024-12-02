eefcb9aed9e193dbcacfcece41ae2d2cb696b4e9
MCS\NBO <Nataraj.Boina@spacewell.com>
2024-12-03 00:12:46 +0530

    fix time

--- src/utils.tsx
+++ src/utils.tsx
@@ -3,11 +3,10 @@ import moment from "moment";
 export const toMinutes = (timeInMilliseconds: number) => {
   //Show time in minutes and seconds conditionally
   const duration = moment.duration(timeInMilliseconds);
-  const minutes = duration.minutes();
-  const seconds = duration.seconds();
-  return duration.asMilliseconds() > 0
-    ? `${minutes} minutes ${seconds} seconds`
-    : "In Progress";
+  const hours = duration.hours() > 0 ? `${duration.hours()} hours` : "";
+  const minutes = duration.minutes() > 0 ? `${duration.minutes()} minutes` : "";
+  const seconds = duration.seconds() > 0 ? `${duration.seconds()} seconds` : "";
+  return `${hours} ${minutes} ${seconds}`;
 };
 
 export const toCompleted = (completed: boolean) => {
